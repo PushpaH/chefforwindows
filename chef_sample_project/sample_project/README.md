b3c564f0709e6dcff0132e6df57381a6d17f4681
Pushpa <pushpa@ekriegers.com>
2023-02-03 21:20:23 +0100

    Add sample cookbook

---
+++ chef_sample_project/sample_project/README.md
@@ -0,0 +1,4 @@
+# sample_project
+
+TODO: Enter the cookbook description here.
+
---
+++ chef_sample_project/sample_project/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: sample_project
+# Recipe:: default
+#
+# Copyright:: 2023, The Authors, All Rights Reserved.
